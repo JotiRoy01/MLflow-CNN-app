95fb43880104990733ba7e7f7e6ed74be2775170
JotiRoy01 <jotiroygit@gmail.com>
2023-06-25 23:36:41 +0600

    final update

--- src/main.py
+++ src/main.py
@@ -22,6 +22,7 @@ def main():
     with mlflow.start_run() as run :
         mlflow.run(".","get_data",env_manager="local")
         mlflow.run(".","base_model_creation",env_manager="local")
+        mlflow.run(".","training",env_manager="local")
 
 
 
